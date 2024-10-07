c412dc698e43cf9555142a8264ea62ce49760364
rmgraham45 <32076943+rmgraham45@users.noreply.github.com>
2024-10-06 21:38:04 -0400

    camera ui

--- UI/Camera.cs
+++ UI/Camera.cs
@@ -1,18 +1,23 @@
 using Godot;
 using System;
 
-public partial class Start : Node2D
+public partial class Camera : Camera2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
-	{	
-		SceneManager.Instance.AddScene("res://WorldScenes/Background.tscn");
-		SceneManager.Instance.AddControl("res://UI/MainUI.tscn");
+	{
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-
+		if (Input.IsActionPressed("Up"))
+		{
+			this.Position = this.Position - new Vector2(0, 5);
+		}
+		if (Input.IsActionPressed("Down"))
+		{
+			this.Position = this.Position + new Vector2(0, 5);
+		}
 	}
 }
