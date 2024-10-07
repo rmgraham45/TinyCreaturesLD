ce10e6a201cfe0221b0450f5716bcda809aecca2
rmgraham45 <32076943+rmgraham45@users.noreply.github.com>
2024-10-07 15:40:25 -0400

    music

--- WorldScenes/Start.cs
+++ WorldScenes/Start.cs
@@ -10,6 +10,7 @@ public partial class Start : Node2D
 		SceneManager.Instance.AddScene("res://WorldScenes/Banner.tscn", new Vector2(450, 946));
 		SceneManager.Instance.AddControl("res://UI/MainUI.tscn");
 		SceneManager.Instance.AddControl("res://WorldScenes/BestiaryUI.tscn");
+		SceneManager.Instance.AddControl("res://WorldScenes/Music.tscn");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
